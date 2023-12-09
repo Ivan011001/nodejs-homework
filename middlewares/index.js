d388d0b03ce922b07ebb5e8684f45583dd90a5f4
Ivan011001 <126008836+Ivan011001@users.noreply.github.com>
2023-12-09 23:14:40 +0200

    avatars-commit

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,9 +1,11 @@
 const authenticate = require("./authenticate");
 const isValidBody = require("./isValidBody");
 const isValidID = require("./isValidID");
+const upload = require("./upload");
 
 module.exports = {
   authenticate,
   isValidBody,
   isValidID,
+  upload,
 };
