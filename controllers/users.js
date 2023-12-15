f103c8e100b5d7cd6aae48745db0e3febdc136a8
Ivan011001 <126008836+Ivan011001@users.noreply.github.com>
2023-12-15 13:05:57 +0200

    Hw05 avatars (#4)
    * avatars-commit
    
    * test-login-controller

--- controllers/users.js
+++ controllers/users.js
@@ -1,7 +1,14 @@
 const bCrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const gravatar = require("gravatar");
+const Jimp = require("jimp");
+const path = require("path");
+const fs = require("fs/promises");
+const avatarsDir = path.join(process.cwd(), "public", "avatars");
+
 require("dotenv").config();
 const SECRET = process.env.SECRET;
+
 const User = require("../models/user");
 const { ctrlWrapper, HttpError } = require("../helpers");
 
@@ -14,7 +21,8 @@ const register = async (req, res) => {
   }
 
   const hashPassword = await bCrypt.hash(password, 10);
-  const newUser = await User.create({ ...req.body, password: hashPassword });
+  const avatarURL = gravatar.url(email);
+  const newUser = await User.create({ ...req.body, password: hashPassword, avatarURL });
 
   res.status(201).json({
     user: {
@@ -75,6 +83,23 @@ const updateSubscription = async (req, res) => {
   });
 };
 
+const updateAvatar = async (req, res) => {
+  const { id } = req.user;
+  const { path: tempUpload, originalname } = req.file;
+
+  const fileName = `${id}_${originalname}`;
+  const finalUpload = path.join(avatarsDir, fileName);
+  await fs.rename(tempUpload, finalUpload);
+
+  const image = await Jimp.read(finalUpload);
+  image.resize(250, 250).write(finalUpload);
+
+  const avatarURL = path.join("avatars", fileName);
+  await User.findByIdAndUpdate(id, { avatarURL });
+
+  res.json({ avatarURL });
+};
+
 const logout = async (req, res) => {
   const { id } = req.user;
   await User.findByIdAndUpdate(id, { $unset: { token: 1 } });
@@ -87,5 +112,6 @@ module.exports = {
   login: ctrlWrapper(login),
   current: ctrlWrapper(current),
   updateSubscription: ctrlWrapper(updateSubscription),
+  updateAvatar: ctrlWrapper(updateAvatar),
   logout: ctrlWrapper(logout),
 };
--- routes/api/users.js
+++ routes/api/users.js
@@ -6,7 +6,7 @@ const {
   loginSchema,
   updateUserSubscription,
 } = require("../../validations/usersSchemas");
-const { isValidBody, authenticate } = require("../../middlewares");
+const { isValidBody, authenticate, upload } = require("../../middlewares");
 
 const router = Router();
 
@@ -14,11 +14,12 @@ router.post("/register", isValidBody(registerSchema), ctrl.register);
 router.post("/login", isValidBody(loginSchema), ctrl.login);
 router.get("/current", authenticate, ctrl.current);
 router.patch(
-  "/current/subscription",
+  "/subscription",
   authenticate,
   isValidBody(updateUserSubscription),
   ctrl.updateSubscription
 );
+router.patch("/avatars", authenticate, upload.single("avatar"), ctrl.updateAvatar);
 router.post("/logout", authenticate, ctrl.logout);
 
 module.exports = router;
